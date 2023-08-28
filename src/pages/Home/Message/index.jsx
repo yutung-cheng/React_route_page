d3082d6f3e6f72911176943a7ea9ed04779e30ab
yutung-cheng <s1001913@gmail.com>
2023-08-27 20:44:31 -0400

    final project

--- src/pages/Home/Message/index.jsx
+++ src/pages/Home/Message/index.jsx
@@ -1,40 +1,56 @@
-import React, { Component } from "react";
-import { Link, Outlet } from "react-router-dom";
+import React from "react";
+import { Link, Outlet, useNavigate } from "react-router-dom";
 
-export default class Message extends Component {
-  state = {
-    messageArr: [
-      { id: "01", title: "Message 1" },
-      { id: "02", title: "Message 2" },
-      { id: "03", title: "Message 3" },
-    ],
+export default function Message() {
+  const messageArr = [
+    { id: "01", title: "Message 1" },
+    { id: "02", title: "Message 2" },
+    { id: "03", title: "Message 3" },
+  ];
+
+  const navigate = useNavigate();
+
+  const navigateShow = (id, title, replace) => {
+    navigate("detail", { state: { id: id, title: title }, replace: replace });
   };
-  render() {
-    const { messageArr } = this.state;
-    return (
-      <div>
-        <ul>
-          {messageArr.map((msgObj) => {
-            //...
-            return (
-              <li key={msgObj.id}>
-                <Link
-                  ///Use `useParams` to pass data.
-                  to={`detail/${msgObj.id}/${msgObj.title}`}
-                  ///Use `useLocation` to pass data.
-                  // to={"detail"}
-                  // state={{ id: msgObj.id, title: msgObj.title }}
-                  replace //Instead of going back, replace the browser history route.
-                >
-                  {msgObj.title}
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
-        <hr />
-        <Outlet />
-      </div>
-    );
-  }
+
+  return (
+    <div>
+      <ul>
+        {messageArr.map((msgObj) => {
+          return (
+            <li key={msgObj.id}>
+              <Link
+                ///Use `useParams` to pass data.
+                to={`detail/${msgObj.id}/${msgObj.title}`}
+                ///Use `useLocation` to pass data.
+                // to={"detail"}
+                // state={{ id: msgObj.id, title: msgObj.title }}
+                // replace //Instead of going back, replace the browser history route.
+              >
+                {msgObj.title}
+              </Link>
+              &nbsp;
+              <button
+                className="button"
+                onClick={() => navigateShow(msgObj.id, msgObj.title, false)}
+              >
+                Push
+              </button>{" "}
+              &nbsp;
+              <button
+                className="button"
+                onClick={() => navigateShow(msgObj.id, msgObj.title, true)}
+              >
+                Replace
+              </button>{" "}
+              &nbsp;
+            </li>
+          );
+        })}
+      </ul>
+      <hr />
+      <Outlet />
+    </div>
+  );
 }
